0087a25cefcddf39e02fe26439563dd9194e7c5d
hbkuk <109803585+hbkuk@users.noreply.github.com>
2024-03-24 10:13:13 +0900

    test: 회원 서비스 레이어에 대한 예외 테스트 케이스 추가

--- src/test/java/com/shop/core/member/application/MemberServiceTest.java
+++ src/test/java/com/shop/core/member/application/MemberServiceTest.java
@@ -1,20 +1,24 @@
 package com.shop.core.member.application;
 
 import com.shop.common.annotation.ApplicationTest;
+import com.shop.common.exception.ErrorType;
 import com.shop.core.member.application.dto.MemberRequest;
 import com.shop.core.member.application.dto.MemberResponse;
 import com.shop.core.member.domain.Member;
 import com.shop.core.member.domain.MemberRepository;
 import com.shop.core.member.domain.Status;
 import com.shop.core.member.domain.Type;
+import com.shop.core.member.exception.DuplicateEmailException;
+import com.shop.core.member.exception.NotFoundMemberException;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
-import static com.shop.core.member.fixture.MemberFixture.브라운;
-import static com.shop.core.member.fixture.MemberFixture.스미스;
+import static com.shop.core.member.fixture.MemberFixture.*;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 
 @DisplayName("회원 서비스 레이어 테스트")
 @ApplicationTest
@@ -34,13 +38,13 @@ public class MemberServiceTest {
 
         @Nested
         class 성공 {
+
             /**
              * Given 회원을 생성한다.
              * When  생성한 회원을 조회할 경우
              * Then  회원이 조회된다.
              */
             @Test
-
             @DisplayName("회원을 생성한다.")
             void 회원_생성() {
                 // given
@@ -61,7 +65,33 @@ public class MemberServiceTest {
         @Nested
         class 실패 {
 
-            // TODO: 예외 케이스 추가
+            @BeforeEach
+            void 사전_회원_생성() {
+                memberService.createMember(MemberRequest.createOf(스미스.이메일, 스미스.비밀번호, 스미스.나이, Type.NORMAL, Status.ACTIVE));
+                memberService.createMember(MemberRequest.createOf(존슨.이메일, 존슨.비밀번호, 존슨.나이, Type.NORMAL, Status.ACTIVE));
+                memberService.createMember(MemberRequest.createOf(윌리엄스.이메일, 윌리엄스.비밀번호, 윌리엄스.나이, Type.NORMAL, Status.ACTIVE));
+                memberService.createMember(MemberRequest.createOf(브라운.이메일, 브라운.비밀번호, 브라운.나이, Type.NORMAL, Status.ACTIVE));
+                memberService.createMember(MemberRequest.createOf(잭슨.이메일, 잭슨.비밀번호, 잭슨.나이, Type.NORMAL, Status.ACTIVE));
+            }
+
+            /**
+             * Given 여러 회원을 생성한다.
+             * When  기존 생성한 회원의 이메일과 동일한 회원을 생성할 경우
+             * Then  회원을 생성할 수 없다.
+             */
+            @Test
+            @DisplayName("회원을 생성할 수 없다.")
+            void 회원_생성() {
+                // given
+                MemberRequest 기존_브라운과_동일한_이메일의_회원 = MemberRequest.createOf(브라운.이메일, "password1!", 20, Type.NORMAL, Status.ACTIVE);
+
+                // when, then
+                assertThatExceptionOfType(DuplicateEmailException.class)
+                        .isThrownBy(() -> {
+                            memberService.createMember(기존_브라운과_동일한_이메일의_회원);
+                        }).withMessageMatching(ErrorType.DUPLICATE_MEMBER_EMAIL.getMessage());
+
+            }
         }
 
     }
@@ -71,6 +101,7 @@ public class MemberServiceTest {
 
         @Nested
         class 성공 {
+
             /**
              * Given 회원을 생성한다.
              * When  생성된 회원의 번호로 회원을 조회할 경우
@@ -94,7 +125,20 @@ public class MemberServiceTest {
 
         @Nested
         class 실패 {
-            // TODO: 예외 케이스 추가
+            /**
+             * When  생성되지 않은 회원의 번호로 회원을 조회할 경우
+             * Then  회원을 조회할 수 없다.
+             */
+            @Test
+            @DisplayName("회원을 조회할 수 없다.")
+            void 회원_조회() {
+                // given
+                assertThatExceptionOfType(NotFoundMemberException.class)
+                        .isThrownBy(() -> {
+                            memberService.findMemberById(99L);
+                        })
+                        .withMessageMatching(ErrorType.NOT_FOUND_MEMBER.getMessage());
+            }
         }
 
     }
@@ -131,11 +175,6 @@ public class MemberServiceTest {
             }
 
         }
-
-        @Nested
-        class 실패 {
-
-        }
     }
 
     @Nested
@@ -150,7 +189,7 @@ public class MemberServiceTest {
              * Then  회원의 상태가 '삭제' 상태로 변경된다.
              */
             @Test
-            @DisplayName("회원을 수정한다.")
+            @DisplayName("회원을 삭제한다.")
             void 회원_수정() {
                 // given
                 MemberRequest 스미스_회원_생성_요청 = MemberRequest.createOf(스미스.이메일, 스미스.비밀번호, 스미스.나이, Type.NORMAL, Status.ACTIVE);
@@ -164,11 +203,6 @@ public class MemberServiceTest {
 
                 assertThat(스미스_회원_상태.getStatus()).isEqualTo(Status.WITHDRAWN);
             }
-
-        }
-
-        @Nested
-        class 실패 {
 
         }
     }
