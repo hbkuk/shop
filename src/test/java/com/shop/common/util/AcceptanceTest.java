d1d66a7f01cc8a5fab736c1c8c94f2de99a6f308
hbkuk <109803585+hbkuk@users.noreply.github.com>
2024-04-19 22:39:45 +0900

    docs: test에서 사용하는 resource 파일 이동
    fix: import 수정

--- src/test/java/com/shop/common/util/AcceptanceTest.java
+++ src/test/java/com/shop/common/util/AcceptanceTest.java
@@ -1,9 +1,6 @@
 package com.shop.common.util;
 
-import io.restassured.RestAssured;
-import org.junit.jupiter.api.BeforeEach;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.test.context.jdbc.Sql;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
