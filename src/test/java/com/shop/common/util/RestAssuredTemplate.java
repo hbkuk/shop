4a556daf3c681f21b5608718c5ec1d3fa9253738
hbkuk <109803585+hbkuk@users.noreply.github.com>
2024-05-10 11:00:23 +0900

    refactor: RestAssured 템플릿을 사용한 Address 인수 테스트 리팩토링

--- src/test/java/com/shop/common/util/RestAssuredTemplate.java
+++ src/test/java/com/shop/common/util/RestAssuredTemplate.java
@@ -9,6 +9,16 @@ import static io.restassured.RestAssured.given;
 
 public class RestAssuredTemplate {
 
+    public static ExtractableResponse<Response> get_요청_토큰_포함(String 토큰_정보, String 경로, HttpStatus 예상하는_상태코드) {
+        return given().log().all()
+                .header("Authorization", 토큰_정보)
+                .when()
+                .get(경로)
+                .then().log().all()
+                .statusCode(예상하는_상태코드.value())
+                .extract();
+    }
+
     public static ExtractableResponse<Response> get_요청_토큰_포함(String 토큰_정보, String 경로, Object 파라미터, HttpStatus 예상하는_상태코드) {
         return given().log().all()
                 .header("Authorization", 토큰_정보)
@@ -77,10 +87,31 @@ public class RestAssuredTemplate {
                 .extract();
     }
 
+    public static ExtractableResponse<Response> put_요청_토큰_포함(String 토큰_정보, String 경로, Object 파라미터, HttpStatus 예상하는_상태코드) {
+        return given().log().all()
+                .header("Authorization", 토큰_정보)
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .when()
+                .put(경로, 파라미터)
+                .then().log().all()
+                .statusCode(예상하는_상태코드.value())
+                .extract();
+    }
+
     public static ExtractableResponse<Response> put_요청_토큰_미포함(String 경로, Object 파라미터, Object 요청_정보, HttpStatus 예상하는_상태코드) {
         return given().log().all()
                 .contentType(MediaType.APPLICATION_JSON_VALUE)
                 .body(요청_정보)
+                .when()
+                .put(경로, 파라미터)
+                .then().log().all()
+                .statusCode(예상하는_상태코드.value())
+                .extract();
+    }
+
+    public static ExtractableResponse<Response> put_요청_토큰_미포함(String 경로, Object 파라미터, HttpStatus 예상하는_상태코드) {
+        return given().log().all()
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
                 .when()
                 .put(경로, 파라미터)
                 .then().log().all()
--- src/test/java/com/shop/core/address/step/AddressSteps.java
+++ src/test/java/com/shop/core/address/step/AddressSteps.java
@@ -5,76 +5,64 @@ import io.restassured.path.json.JsonPath;
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 
 import static com.shop.common.util.HttpResponseUtil.getCreatedLocationId;
-import static io.restassured.RestAssured.given;
+import static com.shop.common.util.RestAssuredTemplate.*;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class AddressSteps {
 
     public static ExtractableResponse<Response> 주소록_등록_요청_토큰_포함(AddressRequest 주소록_등록_요청_정보, String 토큰) {
-        return given().log().all()
-                .header("Authorization", 토큰)
-                .contentType(MediaType.APPLICATION_JSON_VALUE)
-                .body(주소록_등록_요청_정보)
-                .when()
-                .post("/addresses")
-                .then().log().all()
-                .statusCode(HttpStatus.CREATED.value())
-                .extract();
+        return post_요청_토큰_포함(토큰, "/addresses", 주소록_등록_요청_정보, HttpStatus.CREATED);
     }
 
     public static void 주소록_정보_확인(AddressRequest 확인할_주소록_정보, String 토큰) {
-        JsonPath 응답_정보 = given().log().all()
-                .header("Authorization", 토큰)
-                .contentType(MediaType.APPLICATION_JSON_VALUE)
-                .when()
-                .get("/addresses")
-                .then().log().all()
-                .statusCode(HttpStatus.OK.value())
-                .extract()
-                .jsonPath();
+        ExtractableResponse<Response> 응답 = get_요청_토큰_포함(토큰, "/addresses", HttpStatus.OK);
 
-        주소록_일치_확인(확인할_주소록_정보, 응답_정보);
+        주소록_일치_확인(확인할_주소록_정보, 응답.jsonPath());
     }
 
     public static void 주소록_등록_요청_토큰_미포함(AddressRequest 주소록_등록_요청_정보) {
-        given().log().all()
-                .contentType(MediaType.APPLICATION_JSON_VALUE)
-                .body(주소록_등록_요청_정보)
-                .when()
-                .post("/addresses")
-                .then().log().all()
-                .statusCode(HttpStatus.UNAUTHORIZED.value());
+        post_요청_토큰_미포함("/addresses", 주소록_등록_요청_정보, HttpStatus.UNAUTHORIZED);
     }
 
     public static void 주소록_수정_요청_토큰_포함(AddressRequest 주소록_수정_요청_정보, ExtractableResponse<Response> 주소록_등록_요청_응답, String 토큰) {
-        given().log().all()
-                .header("Authorization", 토큰)
-                .contentType(MediaType.APPLICATION_JSON_VALUE)
-                .body(주소록_수정_요청_정보)
-                .when()
-                .put("/addresses/{id}", getCreatedLocationId(주소록_등록_요청_응답))
-                .then().log().all()
-                .statusCode(HttpStatus.OK.value())
-                .extract()
-                .jsonPath();
+        put_요청_토큰_포함(토큰, "/addresses/{id}", getCreatedLocationId(주소록_등록_요청_응답), 주소록_수정_요청_정보, HttpStatus.OK);
     }
 
     public static void 주소록_수정_확인(AddressRequest 확인할_주소록_정보, String 토큰) {
-        JsonPath 응답_정보 = given().log().all()
-                .header("Authorization", 토큰)
-                .contentType(MediaType.APPLICATION_JSON_VALUE)
-                .when()
-                .get("/addresses")
-                .then().log().all()
-                .statusCode(HttpStatus.OK.value())
-                .extract()
-                .jsonPath();
+        ExtractableResponse<Response> 응답 = get_요청_토큰_포함(토큰, "/addresses", HttpStatus.OK);
 
         // then
-        주소록_일치_확인(확인할_주소록_정보, 응답_정보);
+        주소록_일치_확인(확인할_주소록_정보, 응답.jsonPath());
+    }
+
+    public static void 주소록_수정_요청_토큰_미포함(AddressRequest 주소록_수정_요청_정보, ExtractableResponse<Response> 주소록_등록_요청_응답) {
+        put_요청_토큰_미포함("/addresses/{id}", getCreatedLocationId(주소록_등록_요청_응답), 주소록_수정_요청_정보, HttpStatus.UNAUTHORIZED);
+    }
+
+    public static void 기본_주소록_변경_요청_토큰_포함(ExtractableResponse<Response> 주소록_등록_요청_응답, String 토큰_정보) {
+        put_요청_토큰_포함(토큰_정보, "/addresses/default/{id}", getCreatedLocationId(주소록_등록_요청_응답), HttpStatus.OK);
+    }
+
+    public static void 기본_주소록_확인(ExtractableResponse<Response> 주소록_등록_요청_응답, String 토큰) {
+        ExtractableResponse<Response> 응답 = get_요청_토큰_포함(토큰, "/addresses/{id}", getCreatedLocationId(주소록_등록_요청_응답), HttpStatus.OK);
+
+        기본_주소록_확인(응답.jsonPath(), true);
+    }
+
+    public static void 기본_주소록_변경_요청_토큰_미포함(ExtractableResponse<Response> 주소록_등록_요청_응답) {
+        put_요청_토큰_미포함("/addresses/default/{id}", getCreatedLocationId(주소록_등록_요청_응답), HttpStatus.UNAUTHORIZED);
+    }
+
+    public static void 기본_주소록_아님_확인(ExtractableResponse<Response> 주소록_등록_요청_응답, String 토큰) {
+        ExtractableResponse<Response> 응답 = get_요청_토큰_포함(토큰, "/addresses/{id}", getCreatedLocationId(주소록_등록_요청_응답), HttpStatus.OK);
+
+        기본_주소록_확인(응답.jsonPath(), false);
+    }
+
+    private static void 기본_주소록_확인(JsonPath jsonPath, boolean expected) {
+        assertThat(jsonPath.getBoolean("isDefault")).isEqualTo(expected);
     }
 
     private static void 주소록_일치_확인(AddressRequest 확인할_주소록_정보, JsonPath jsonPath) {
@@ -82,68 +70,6 @@ public class AddressSteps {
         assertThat(jsonPath.getList("detailedAddress", String.class)).containsExactly(확인할_주소록_정보.getDetailedAddress());
         assertThat(jsonPath.getList("description", String.class)).containsExactly(확인할_주소록_정보.getDescription());
         assertThat(jsonPath.getList("isDefault", boolean.class)).containsExactly(확인할_주소록_정보.getIsDefault());
-    }
-
-    public static void 주소록_수정_요청_토큰_미포함(AddressRequest 주소록_수정_요청_정보, ExtractableResponse<Response> 주소록_등록_요청_응답) {
-        given().log().all()
-                .contentType(MediaType.APPLICATION_JSON_VALUE)
-                .body(주소록_수정_요청_정보)
-                .when()
-                .put("/addresses/{id}", getCreatedLocationId(주소록_등록_요청_응답))
-                .then().log().all()
-                .statusCode(HttpStatus.UNAUTHORIZED.value())
-                .extract();
-    }
-
-    public static void 기본_주소록_변경_요청_토큰_포함(ExtractableResponse<Response> 주소록_등록_요청_응답, String 토큰_정보) {
-        given().log().all()
-                .header("Authorization", 토큰_정보)
-                .when()
-                .put("/addresses/default/{id}", getCreatedLocationId(주소록_등록_요청_응답))
-                .then().log().all()
-                .statusCode(HttpStatus.OK.value())
-                .extract();
-    }
-
-    public static void 기본_주소록_확인(ExtractableResponse<Response> 주소록_등록_요청_응답, String 토큰) {
-        JsonPath jsonPath = given().log().all()
-                .header("Authorization", 토큰)
-                .contentType(MediaType.APPLICATION_JSON_VALUE)
-                .when()
-                .get("/addresses/{id}", getCreatedLocationId(주소록_등록_요청_응답))
-                .then().log().all()
-                .statusCode(HttpStatus.OK.value())
-                .extract()
-                .jsonPath();
-
-        기본_주소록_확인(jsonPath, true);
-    }
-
-    private static void 기본_주소록_확인(JsonPath jsonPath, boolean expected) {
-        assertThat(jsonPath.getBoolean("isDefault")).isEqualTo(expected);
-    }
-
-    public static void 기본_주소록_변경_요청_토큰_미포함(ExtractableResponse<Response> 주소록_등록_요청_응답) {
-        given().log().all()
-                .when()
-                .put("/addresses/default/{id}", getCreatedLocationId(주소록_등록_요청_응답))
-                .then().log().all()
-                .statusCode(HttpStatus.UNAUTHORIZED.value())
-                .extract();
-    }
-
-    public static void 기본_주소록_아님_확인(ExtractableResponse<Response> 주소록_등록_요청_응답, String 토큰) {
-        JsonPath jsonPath = given().log().all()
-                .header("Authorization", 토큰)
-                .contentType(MediaType.APPLICATION_JSON_VALUE)
-                .when()
-                .get("/addresses/{id}", getCreatedLocationId(주소록_등록_요청_응답))
-                .then().log().all()
-                .statusCode(HttpStatus.OK.value())
-                .extract()
-                .jsonPath();
-
-        기본_주소록_확인(jsonPath, false);
     }
 
 }
